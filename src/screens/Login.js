25654fdbce786a4bb123909444d7d7803b5432ca
MUNSCH <thomas-munsch_student2021@wilder.school>
2022-02-17 12:41:18 +0100

    created loginPage content with logo, form and button + css and made responsive

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,6 +1,34 @@
+import BTHTLogo from "../assets/BTHT-Blue_edited.webp";
+
+import "./styles/Login.css";
+
 const Login = () => {
-    return <div> Login </div>
+  return (
+    <div className="loginPage">
+      <div className="loginBackground"></div>
+      <div className="loginContainer">
+        <img src={BTHTLogo} alt="btht-logo" id="loginLogo" />
+        <div className="loginWrapper">
+          <div className="loginInput">
+            <label htmlFor="userName" className="loginLabel">
+              Username
+            </label>
+            <input type="text" id="userName" placeholder="username" />
+          </div>
+          <div className="loginInput">
+            <label htmlFor="password" className="loginLabel">
+              Password
+            </label>
+            <input type="text" id="password" placeholder="password" />
+          </div>
+          <div className="loginBtnWrapper">
+            <button id="loginBtn">ENTER</button>
+          </div>
+        </div>
+      </div>
+      <div className="loginBackground backgroundRight"></div>
+    </div>
+  );
+};
 
-}
-
-export default Login
+export default Login;
