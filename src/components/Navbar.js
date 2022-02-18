e526e058c1509542026aaf04e739554d7ec4c2c8
MUNSCH <thomas-munsch_student2021@wilder.school>
2022-02-18 12:23:40 +0100

    created navbar content and css, removed text-align: center from App.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,34 @@
+import Chevron from "../assets/chevron.svg";
+import Client from "../assets/client.png";
+import Expert from "../assets/expert.png";
+import Project from "../assets/project.png";
+
+import "./styles/Navbar.css";
+
 const Navbar = () => {
-    return <div> Navbar </div>
+  return (
+    <div className="navbar">
+      <div className="navbarOpener">
+        <img src={Chevron} alt="chevron" />
+      </div>
+      <div className="navbarLinksContainer">
+        <ul className="navbarLinksWrapper">
+          <li className="navbarLink">
+            <img src={Project} alt="projectLogo" className="navbarLinkLogo" />
+            <span className="navbarLinkName">Projects</span>
+          </li>
+          <li className="navbarLink">
+            <img src={Client} alt="clientLogo" className="navbarLinkLogo" />
+            <span className="navbarLinkName">Clients</span>
+          </li>
+          <li className="navbarLink">
+            <img src={Expert} alt="expertLogo" className="navbarLinkLogo" />
+            <span className="navbarLinkName">Experts</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default Navbar
+export default Navbar;
