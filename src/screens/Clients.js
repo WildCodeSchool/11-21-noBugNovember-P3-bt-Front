2db42d72cae9967a5518b6515965825dd87ef447
JulesCharles <julees.charles@gmail.com>
2022-03-07 16:13:06 +0100

    Création bouton Ajouter link page Clients et PageClient.js

--- src/screens/Clients.js
+++ src/screens/Clients.js
@@ -1,13 +1,15 @@
-import TabClients from "../components/TabClients";
-import "./styles/Clients.css";
+import TabClients from '../components/TabClients'
+import './styles/Clients.css'
+import { Link } from 'react-router-dom'
 
 const Clients = () => {
   return (
-    <div className="tabContainer">
+    <div className='tabContainer'>
       <h1>Clients</h1>
+      <Link to='/pageClient'>Ajouter</Link>
       <TabClients />
     </div>
-  );
-};
+  )
+}
 
-export default Clients;
+export default Clients
