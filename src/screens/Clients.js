39e3273312225eeef0969b27354492a38b7a3ea0
Anouchka <anouchka.maurer@protonmail.com>
2022-03-21 15:37:51 +0100

    application nouveau bouton experts au client en passant le css dans App

--- src/screens/Clients.js
+++ src/screens/Clients.js
@@ -1,17 +1,36 @@
 import TabClients from '../components/TabClients'
+import { useState } from 'react'
 import './styles/Clients.css'
 import { Link } from 'react-router-dom'
 
 const Clients = (props) => {
+  const [clientSelection, setClientSelection] = useState([])
   return (
     <div className='tabContainer'>
       <div className='titleButtonContainer'>
         <h1 className='pageTitle'>Clients</h1>
-        <Link to='/pageClient'>
-          <button className='buttonAjout'>+ Add new client</button>
-        </Link>
+        <TabClients />
+        <div className='buttonAjoutContainer'>
+          <div>
+            <Link to='/pageClient'>
+              <button className='buttonAjout'>ADD</button>
+            </Link>
+          </div>
+          {clientSelection.length !== 0 ? (
+            <div>
+              <Link to={`/pageClientEdit/${clientSelection}`}>
+                <button className='buttonAjout'>EDIT</button>
+              </Link>
+            </div>
+          ) : (
+            <div>
+              <a href='#'>
+                <button className='buttonAjout'>EDIT</button>
+              </a>
+            </div>
+          )}
+        </div>
       </div>
-      <TabClients />
     </div>
   )
 }
