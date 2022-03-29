854dd720814445c11dce9fec2adbd4142ae7f0ff
MUNSCH <thomas-munsch_student2021@wilder.school>
2022-03-29 16:19:49 +0200

    changed CA into TO

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChartLine } from "@fortawesome/free-solid-svg-icons";
 import "./styles/Header.css";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   const [stats, setStats] = useState([]);
@@ -15,10 +16,12 @@ const Header = () => {
       .then((res) => res.data)
       .then((res) => setStats(res));
   }, []);
-  console.log("stats", stats);
+
   return (
     <div className="headerContainer">
-      <img src={BTHTLogo} alt="logo_BTHT" id="headerLogo" />
+      <NavLink to="../">
+        <img src={BTHTLogo} alt="logo_BTHT" id="headerLogo" />
+      </NavLink>
       <div className="headerStatsContainer">
         {/* <img src={GraphLogo} alt="graph_logo" id="headerGraphLogo" /> */}
         <div className="headerStats">
@@ -35,16 +38,16 @@ const Header = () => {
             />
             {stats.ongoingProjects} Ongoing Projects
           </p>
-          <p className="statItem">${stats.OngoingCA} Ongoing CA </p>
+          <p className="statItem">€{stats.OngoingCA} Ongoing TO </p>
           {/* <p className="statItem">{stats.doneprojects} Done Projects</p> */}
           <p className="statItem">{stats.totalProjects} Projects</p>
           <p className="statItem">{stats.totalExperts} Experts</p>
-          <p className="statItem">{stats.DoneCA} CA</p>
+          <p className="statItem">€{stats.DoneCA} Total TO</p>
           {/* <p className="statItem">
             {stats.expertsWorked} Participating Experts
           </p> */}
           {/* <p className="statItem">{stats.totalClients} Clients</p> */}
-          <p className="statItem">${stats.CostExperts} Experts Cost</p>
+          <p className="statItem">€{stats.CostExperts} Total Expert Cost</p>
         </div>
       </div>
     </div>
